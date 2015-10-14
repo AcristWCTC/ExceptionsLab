012079d645aa2170f049b56af4a0d140ed0489ac
Adam <Adam@10.30.21.175>
2015-10-14 17:06:49 -0500

    Finished

--- src/lab0/Employee.java
+++ src/lab0/Employee.java
@@ -1,6 +1,10 @@
 package lab0;
 
 import java.util.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * In this challenge you need to address the probability that at some point the
@@ -19,14 +23,17 @@ import java.util.Date;
  * @version 1.00
  */
 public class Employee {
-
+    SimpleDateFormat fmt = new SimpleDateFormat("MM-dd-yyyy");
+    
     public static final int MAX_VACATION_DAYS = 28;
     private String firstName;
     private String lastName;
     private String ssn;
     private Date hireDate;
     private int daysVacation;
-
+    private Date today = Calendar.getInstance().getTime();
+    
+    
     public Employee() {
         firstName = "Unknown";
         lastName = "Unknown";
@@ -42,14 +49,14 @@ public class Employee {
         this.hireDate = hireDate;
         this.daysVacation = daysVacation;
     }
-
-    public int getDaysVacation() {
+    
+    public final int getDaysVacation() {
         return daysVacation;
     }
 
-    public void setDaysVacation(int daysVacation) throws IllegalArgumentException {
-        if (daysVacation < -30 || daysVacation > 30) {
-            throw new IllegalArgumentException("sorry invalid");
+    public final void setDaysVacation(int daysVacation) throws IllegalArgumentException {
+        if(daysVacation < -30 || daysVacation > 30) {
+            throw new IllegalArgumentException("Sorry vacation days must be between -30 and 30");
         }
         this.daysVacation = daysVacation;
     }
@@ -57,37 +64,38 @@ public class Employee {
     public String getFirstName() {
         return firstName;
     }
-
-    /**
-     * Sets the required First Name.
-     *
-     * @param firstName - May not be null or empty
-     * @throws IllegalArgumentException if first name == null has a length == 0
-     */
+/**
+ * Sets the required first name
+ * @param firstName may not be null but may be empty
+ * @throws IllegalArgumentException if firstName == null || has a length of zero
+ */
     public final void setFirstName(String firstName) throws IllegalArgumentException {
-        if (firstName == null || firstName.length() == 0) {
-            throw new IllegalArgumentException("First Name cannot be null");
-        }
-
+       if (firstName == null || firstName.isEmpty() || "Unknown".equals(firstName)){
+        throw new IllegalArgumentException("First Name cannot be null, empty or Unknown");
+    }
         this.firstName = firstName;
     }
 
-    public Date getHireDate() {
+    public final Date getHireDate() {
         return hireDate;
     }
-
-    public void setHireDate(Date hireDate) {
-
+//Date#before(otherDate) (or) Date#after(otherDate) to compare them. 
+    public final void setHireDate(Date hireDate) throws ParseException {
+        if (hireDate.before(fmt.parse("12-12-2012")) || hireDate.after(today) ){
+        throw new IllegalArgumentException("Employee cannot be hired before the company was founded, or be a future date.");
+    }
         this.hireDate = hireDate;
     }
 
-    public String getLastName() {
+    public final String getLastName() {
         return lastName;
     }
 
-    public void setLastName(String lastName) {
-
-        this.lastName = lastName;
+    public final void setLastName(String lastName) {
+        if ( lastName == null || lastName.isEmpty()){
+        throw new IllegalArgumentException("Last name cannot be null or empty");
+                }
+                this.lastName = lastName;
     }
 
     public final String getSsn() {
@@ -96,27 +104,30 @@ public class Employee {
 
     // 333-33-3333
     // 333333333
-    public void setSsn(String ssn) {
-//        if (ssn == null) {
-//            throw new IllegalArgumentException("First Name cannot be null");
-//        }
-//        if (ssn.length() == 9 || ssn.length() == 11){
-//            String [] parts = ssn.split("-");
-//            if (parts.length == 0){
-//                char[] chars = ssn.toCharArray();
-//                
-//            } else if(parts.length == 3){
-//                for (String s: parts){
-//                    char[] c = s.toCharArray();
-//                    
-//                }
-//            }
-//            
-//            
-//        } else {
-//        throw new IllegalArgumentException();
-//        }
+    public final void setSsn(String ssn) {
+        if (ssn == null){
+            throw new IllegalArgumentException("SSN cannot be Null");
+        }
+        if ( ssn.length() < 9 || ssn.length() > 11 || ssn.length() == 10 ){
+            throw new IllegalArgumentException("The SSN must be 9 or 11 digits long.");
+        }
+        if(ssn.length() == 9 || ssn.length() == 11){
+            String[] parts = ssn.split("-");
+            if (parts.length ==0){
+                char[] chars = ssn.toCharArray();
+            } else if ( parts.length == 3){
+                for (String s : parts){
+                    char[] c = s.toCharArray();
+                }
+            }
+            
+        
+    } else {
+            throw new IllegalArgumentException();
+            }
         this.ssn = ssn;
     }
-
+    
+    
+    
 }
